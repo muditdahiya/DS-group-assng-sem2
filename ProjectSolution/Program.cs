1fcfbc4eff120dc6786c38ac3a56b568a97a5494
Mudit Dahiya <mdtheroxtar@gmail.com>
2022-04-14 00:19:30 -0400

    comments for the team

--- ProjectSolution/Program.cs
+++ ProjectSolution/Program.cs
@@ -15,10 +15,10 @@ while (choice != -1)
     Console.WriteLine("--------------------------------------------------");
     Console.WriteLine("\n" + "What would you like to do?");
 
-    Console.WriteLine("1. Add new customer");
-    Console.WriteLine("2. Checkout customer");
-    Console.WriteLine("3. Take order");
-    Console.WriteLine("4. Calculate tips");
+    Console.WriteLine("1. New customer"); //create customer and assign to table
+    Console.WriteLine("2. Checkout customer"); //generate bill, empty table
+    Console.WriteLine("3. Take order"); //generate order and assign to table
+    Console.WriteLine("4. Calculate tips"); //sum the TipJar
     Console.WriteLine("-1. Close restaurant for the day");
 
     Console.Write("\n" + "Enter option: ");
@@ -39,16 +39,16 @@ while (choice != -1)
     switch (choice)
     {
         case 1:
-            Console.WriteLine("Added new customer to table xyz");
+            Console.WriteLine("");
             break;
         case 2:
-            Console.WriteLine("Generate bill");
+            Console.WriteLine("Implement in progress");
             break;
         case 3:
-            Console.WriteLine("Taking order from table xyz");
+            Console.WriteLine("Implement in progress");
             break;
         case 4:
-            Console.WriteLine("Calculating tips for the dat");
+            Console.WriteLine("Implement in progress");
             break;
         case -1:
             Console.WriteLine("Bye");
