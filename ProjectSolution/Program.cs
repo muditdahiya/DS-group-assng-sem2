7f7fa224f7e17242e5050d2d7564544585b4a3be
Mudit Dahiya <mdtheroxtar@gmail.com>
2022-04-14 00:21:23 -0400

    comments for the team

--- ProjectSolution/Program.cs
+++ ProjectSolution/Program.cs
@@ -19,7 +19,7 @@ while (choice != -1)
     Console.WriteLine("2. Checkout customer"); //generate bill, empty table
     Console.WriteLine("3. Take order"); //generate order and assign to table
     Console.WriteLine("4. Calculate tips"); //sum the TipJar
-    Console.WriteLine("-1. Close restaurant for the day");
+    Console.WriteLine("-1. Close restaurant for the day"); //can use some facade here
 
     Console.Write("\n" + "Enter option: ");
     
@@ -39,7 +39,7 @@ while (choice != -1)
     switch (choice)
     {
         case 1:
-            Console.WriteLine("");
+            Console.WriteLine("Implement in progress");
             break;
         case 2:
             Console.WriteLine("Implement in progress");
