221b52935a426d92b2e438a7db922e38dacd9e8a
Harini Rajarathinam <n01419700@humber.ca>
2022-04-16 07:23:30 -0600

    Hello! Made the Customer functionality with 6 classes.

---
+++ ProjectSolution/CustomerFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//CustomerFactory object should be created in Bill to decide discount amount
+namespace ProjectSolution
+{
+    class CustomerFactory
+    {
+        public Customer GetCustomer(int age, boolean isStudent)
+        {   //if statements to check customer type and redeemable discounts
+            if (isStudent)
+            {
+                return new CustomerTypeStudent();
+                //instance to return CustomerTypeStudent
+            }
+            //Verifying age brackets 
+            else if (age < 12)
+            {
+                return new CustomerTypeKid();
+                //instance to return CustomerTypeKid
+            }
+            else if (age >= 60)
+            {
+                return new CustomerTypeSenior();
+                //instance to return CustomerTypeSenior
+            }
+            return new CustomerTypeRegular();
+        }
+
+    }
+}
---
+++ ProjectSolution/CustomerTypeSenior.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSolution
+{
+    class CustomerTypeSenior : Customer
+    {
+        //GetDiscount returns value of discount %
+        public double GetDiscount()
+        { //Students = 15% discount
+          //0.15 gets multiplied from total sum and that gets deducted from total bill amount 
+            return 0.15;
+        }
+    }
+}
