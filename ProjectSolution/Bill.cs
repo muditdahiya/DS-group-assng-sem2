894d3534c5c77ca6cb988bf78d2a30c8cb16a8e5
Mudit Dahiya <mdtheroxtar@gmail.com>
2022-04-19 12:42:35 -0400

    edited ouput of bill

--- ProjectSolution/Bill.cs
+++ ProjectSolution/Bill.cs
@@ -26,25 +26,25 @@ namespace ProjectSolution
             Console.WriteLine("\nBill for table number " + table.Number);
             // 1. Display the contents of the order and the itemized price calculation
             Console.WriteLine(orderParser.DisplayBill());
-            Console.WriteLine("***********************************");
+            Console.WriteLine("----");
 
             //2. Calcuate Total Amount
             billCalculator.SetStrategy(new SumBillAmountStrategy(order));
             double totalBillAmount = billCalculator.Calculate();
             Console.WriteLine($"Total Amount: {totalBillAmount}");
-            Console.WriteLine("***********************************");
+            Console.WriteLine("----");
 
             //3. Apply Tax
             billCalculator.SetStrategy(new ApplyTaxStrategy(totalBillAmount, 0.13));
             totalBillAmount = billCalculator.Calculate();
             Console.WriteLine($"Total Amount After Applying Taxes: {totalBillAmount}");
-            Console.WriteLine("***********************************");
+            Console.WriteLine("----");
 
             //4. Apply Discount
             billCalculator.SetStrategy(new ApplyDiscountStrategy(totalBillAmount, customer));
             totalBillAmount = billCalculator.Calculate();
             Console.WriteLine($"Total Amount After Applying Discounts: {totalBillAmount}");
-            Console.WriteLine("***********************************");
+            Console.WriteLine("--------------------");
         }
     }
 }
